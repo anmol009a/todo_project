025905e1bc67a1427048d350e2919b385dcb0872
Anmol Sharma <anmolsharma009a@gmail.com>
2024-11-29 12:52:11 +0530

    Define Views todo_app/views.py

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing, creating, updating, and deleting todo items.
+    """
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
