a88203f95b9c81b066fcd6d190acf46ea6a2e72d
Anmol Sharma <anmolsharma009a@gmail.com>
2024-11-29 11:14:35 +0530

    Register the model in todo_app/admin.py

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Todo
 
 # Register your models here.
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'timestamp', 'due_date')
+    list_filter = ('status', 'timestamp', 'due_date')
+    fieldsets = (
+        (None, {'fields': ('title', 'description', 'status')}),
+        ('Additional Info', {'fields': ('due_date', 'tags')}),
+    )
+    readonly_fields = ('timestamp',)
