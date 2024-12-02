2603fb5e294a93b42ee04c3323477156d63002c5
Anmol Sharma <anmolsharma009a@gmail.com>
2024-12-03 03:55:48 +0530

    update settings

--- manage.py
+++ manage.py
@@ -2,11 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from todo_project.settings import base
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
+
+    if base.DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings.local')
+    else:
+        os.environ.setdefault(
+            'DJANGO_SETTINGS_MODULE', 'todo_project.settings.production'
+        )
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
